9419a6cc23917d5211ff890649440409fd71de0d
Ray Nicholus <rnicholus@widen.com>
2013-09-06 22:44:31 -0500

    start of image cross-origin examples

---
+++ src/local/client/image/modifier.js
@@ -0,0 +1,41 @@
+$(function() {
+    $("#logo").on("load", function() {
+        var $canvas = $("#canvas"),
+            context = $canvas[0].getContext("2d");
+
+        context.drawImage($("#logo")[0], 0, 0);
+
+        enableCaching($canvas);
+        enablePainting($canvas, context);
+    });
+
+    function enableCaching($canvas) {
+        $("#cache").click(function() {
+            var dataUrl = $canvas[0].toDataURL();
+            localStorage.setItem("logo", dataUrl);
+        });
+    }
+
+    function enablePainting($canvas, context) {
+        var canvasOffset = $canvas.offset(),
+            drawing = false;
+
+        $canvas.mousemove(function(event) {
+            if (drawing) {
+                context.lineTo(event.pageX - canvasOffset.left,
+                               event.pageY - canvasOffset.top);
+                context.stroke();
+            }
+        })
+            .mousedown(function(event) {
+                drawing = true;
+                context.beginPath();
+                context.moveTo(event.pageX - canvasOffset.left,
+                               event.pageY - canvasOffset.top);
+            });
+
+        $(window).mouseup(function() {
+            drawing = false;
+        });
+    }
+});
